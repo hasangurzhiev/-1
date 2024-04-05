a85018cab0004eadc1b4e0e3cee7a7bebba74820
hasangurzhiev <166034092+hasangurzhiev@users.noreply.github.com>
2024-04-05 14:21:50 +0300

    Add files via upload

---
+++ Анкета/Анкета/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace QuestionnaireApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string color = ((ComboBoxItem)cmbColor.SelectedItem).Content.ToString();
+            string animal = ((ComboBoxItem)cmbAnimal.SelectedItem).Content.ToString();
+            string season = ((ComboBoxItem)cmbSeason.SelectedItem).Content.ToString();
+
+            MessageBox.Show($"Your answers:\nFavorite color: {color}\nFavorite animal: {animal}\nFavorite season: {season}");
+        }
+    }
+}
+
